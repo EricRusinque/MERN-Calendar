2d81f78bae4f1105442378a7228d3e56cc98bc63
unknown <emprenderusinque@hotmail.com>
2022-02-04 00:12:25 +0100

    additional settings to the calendar

--- src/components/calendar/CalendarScreen.jsx
+++ src/components/calendar/CalendarScreen.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { NavBar } from '../ui/NavBar';
 import { Calendar, momentLocalizer } from 'react-big-calendar'
+import { messages } from '../../helpers/calendar-messages';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css'
+import 'moment/locale/es'
+
+moment.locale('es');
 
 const localizer = momentLocalizer(moment);
 
@@ -14,15 +18,31 @@ const events =[{
 }]
 
 export const CalendarScreen = () => {
+
+    const eventStylesGetter = (event, start, isSelected) => {
+        console.log({event, start, isSelected});
+        const style = {
+            backgroundColor: '#367CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white'
+        }
+        return {
+            style
+        }
+    };
+
     return (
         <div className="calendar-screen">
             <NavBar/>
 
             <Calendar 
                 localizer={localizer}
-                events={ events}
+                events={ events }
                 startAccessor="start"
                 endAccessor="end"
+                eventPropGetter={ eventStylesGetter }
             />
         </div>
     );
